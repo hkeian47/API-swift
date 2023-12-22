7ed84de27895a862bfd7b223e9e61b5025a5a977
Hana Keinan <56659307+hkeian47@users.noreply.github.com>
2023-12-21 19:04:44 -0600

    first commit

---
+++ API-swift/API_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_swiftApp.swift
+//  API-swift
+//
+//  Created by Hana Keinan on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct API_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  API-swift
+//
+//  Created by Hana Keinan on 12/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
